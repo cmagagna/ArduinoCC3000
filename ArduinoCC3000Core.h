4df1b1e5126604f62379d2f76d65e038760f9483
Chris Magagna <cmagagna@yahoo.com>
2013-06-23 12:08:36 -0700

    Added comments on CC3000_TINY_DRIVER
    Uncomment the CC3000_TINY_DRIVER option in ArduinoCC3000Core.h to
    reduce Flash and RAM usage.

--- ArduinoCC3000Core.h
+++ ArduinoCC3000Core.h
@@ -38,6 +38,38 @@
 #define USE_HARDWARE_SPI	true
 
 
+
+
+
+/* If you uncomment the line below the library will leave out a lot of the
+   higher level functions but use a lot less memory. From:
+   
+   http://processors.wiki.ti.com/index.php/Tiny_Driver_Support
+   
+  CC3000’s new driver has flexible memory compile options.
+  
+  This feature comes in handy when we want to use a limited RAM size MCU.
+  
+  Using The Tiny Driver Compilation option will create a tiny version of our
+  host driver with lower data, stack and code consumption.
+  
+  By enabling this feature, host driver’s RAM consumption can be reduced to
+  minimum of 251 bytes.
+  
+  The Tiny host driver version will limit the host driver API to the most
+  essential ones.
+  
+  Code size depends on actual APIs used.
+  
+  RAM size depends on the largest packet sent and received.
+  
+  CC3000 can now be used with ultra low cost MCUs, consuming 251 byte of RAM
+  and 2K to 6K byte of code size, depending on the API usage. */
+
+//#define CC3000_TINY_DRIVER	1
+
+
+
                                 
 
 
